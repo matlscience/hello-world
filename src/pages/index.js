a4869162f86187fe7163a1d2ec0e0437511ffef6
Gregory W Douglas <gregory.w.douglas@cheopsconsulting.com>
2020-07-19 20:47:01 -0500

    adding Gatsby project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
-
+import { Link } from "gatsby"
+import Header from "../components/header"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link> 
+      <h2>Gatsby Default Starter</h2>
+      <div>Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.</div>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
+
